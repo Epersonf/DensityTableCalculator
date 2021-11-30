00c7027d5fe8dee53b2e4cf43ca57653de72159f
Eperson <epersoncmxf@gmail.com>
2021-11-30 00:29:04 -0300

    Correction factor, alcoholic degree 20

--- src/scripts/input-getter.ts
+++ src/scripts/input-getter.ts
@@ -10,6 +10,8 @@ function listen() {
   const temperatureUnitInput: HTMLSelectElement = document.querySelector(".temperature-unit-input") as HTMLSelectElement;
   const meResult: HTMLParagraphElement = document.querySelector(".me-result") as HTMLParagraphElement;
   const meResult20: HTMLParagraphElement = document.querySelector(".me-result-20") as HTMLParagraphElement;
+  const alcoholicDegreeOutput: HTMLParagraphElement = document.querySelector(".alcoholic-degree-gv") as HTMLParagraphElement;
+  const correctionFactorOutput: HTMLParagraphElement = document.querySelector(".correction-factor") as HTMLParagraphElement;
   const calculateButton: HTMLButtonElement = document.querySelector(".button-calculate") as HTMLButtonElement;
 
   alcoholDegreeInput.value = alcoholDegree.toString();
@@ -32,8 +34,12 @@ function listen() {
 
   calculateButton.onclick = () => {
     const result = calculate(getAlcoholDegree(), getTemperatureInCelsius());
-    meResult.innerText = `Specific Mass of Alcohol: ${result.specificMass.toFixed(5)}`;
-    meResult20.innerText = `Specific Mass of Alcohol at 20ºc: ${result.specificMass20.toFixed(5)}`;
+    meResult.innerHTML = `Specific Mass of Alcohol: <b style="font-weight: bold; color: red;">${result.specificMass.toFixed(5)}</b>`;
+    meResult20.innerHTML = `Specific Mass of Alcohol at 20ºC: <b style="font-weight: bold; color: red;">${result.specificMass20.toFixed(5)}</b>`;
+    alcoholicDegreeOutput.innerHTML = `Alcoholic degree at 20ºC: <b style="font-weight: bold; color: red;">${(getAlcoholDegree() * (result.specificMass20 / 789.23)).toFixed(5)}</b>`;
+    correctionFactorOutput.innerHTML = `Correction factor: <b style="font-weight: bold; color: red;">${(
+      (result.specificMass / result.specificMass20) * (1 + 0.000036 * (getTemperatureInCelsius() - 20))
+    ).toFixed(5)}</b>`;
   };
 }
 
